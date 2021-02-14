311d923803bc8cb135cece9aab46538e18594105
Brunya <brunya2@icloud.com>
2021-02-15 00:33:30 +0300

    First commit

--- Xylophone-iOS13/Xylophone/ViewController.swift
+++ Xylophone-iOS13/Xylophone/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
     
         playSound(soundName: sender.currentTitle!)
     }
-    
     func playSound(soundName: String ) {
         let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
